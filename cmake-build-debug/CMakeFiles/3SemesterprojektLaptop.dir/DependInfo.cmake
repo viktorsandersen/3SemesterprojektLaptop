853d8a1d1016e4a41f2e72f83e8efa404bb62038
viktorsandersen <71931240+viktorsandersen@users.noreply.github.com>
2021-12-07 17:42:47 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/3SemesterprojektLaptop.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/decoder.cpp" "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/cmake-build-debug/CMakeFiles/3SemesterprojektLaptop.dir/decoder.cpp.obj"
+  "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/main.cpp" "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/cmake-build-debug/CMakeFiles/3SemesterprojektLaptop.dir/main.cpp.obj"
+  "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/processor.cpp" "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/cmake-build-debug/CMakeFiles/3SemesterprojektLaptop.dir/processor.cpp.obj"
+  "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/sampler.cpp" "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/cmake-build-debug/CMakeFiles/3SemesterprojektLaptop.dir/sampler.cpp.obj"
+  "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/toolbox.cpp" "C:/Users/vikto/CLionProjects/3SemesterprojektLaptop/cmake-build-debug/CMakeFiles/3SemesterprojektLaptop.dir/toolbox.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/SFML/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/3SemesterprojektLaptop.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "3SemesterprojektLaptop.exe"
+  "3SemesterprojektLaptop.exe.manifest"
+  "3SemesterprojektLaptop.pdb"
+  "CMakeFiles/3SemesterprojektLaptop.dir/decoder.cpp.obj"
+  "CMakeFiles/3SemesterprojektLaptop.dir/main.cpp.obj"
+  "CMakeFiles/3SemesterprojektLaptop.dir/processor.cpp.obj"
+  "CMakeFiles/3SemesterprojektLaptop.dir/sampler.cpp.obj"
+  "CMakeFiles/3SemesterprojektLaptop.dir/toolbox.cpp.obj"
+  "lib3SemesterprojektLaptop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3SemesterprojektLaptop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
